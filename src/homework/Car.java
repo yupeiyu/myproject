e4feb7edfff3ff091f3a024877369ca4cb0ce6fe
Peiyu <user@es302n06>
2017-03-14 15:20:41 +0800

    carType

---
+++ src/homework/Car.java
@@ -0,0 +1,14 @@
+package homework;
+
+public class Car {
+
+	
+		String type;
+		int cc;
+		public Car(String type , int cc){
+			this.type=type;
+			this.cc=cc;
+
+	}
+
+}
---
+++ src/homework/CarTester.java
@@ -0,0 +1,14 @@
+package homework;
+
+public class CarTester {
+
+	public static void main(String[] args) {
+
+		Car car =new Car("Totyal",1800);
+		System.out.println(car.cc);
+				
+		}
+
+	}
+
+
