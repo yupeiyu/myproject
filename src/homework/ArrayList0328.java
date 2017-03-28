c6b54da5636a2ce7aa77cbd3c365eee177cb1b90
user <user@es302n06>
2017-03-28 15:22:13 +0800

    ArrayList

---
+++ src/homework/ArrayList0328.java
@@ -0,0 +1,20 @@
+package homework;
+
+import java.util.ArrayList;
+
+public class ArrayList0328 {
+
+	public static void main(String[] args) {
+		ArrayList a=new ArrayList();
+		a.add("OMG");
+		a.add("WTF");
+		a.add("BTW");
+		a.set(2, "HAHA");
+		System.out.println(a.size());
+		System.out.println(a);
+		
+		
+
+	}
+
+}
