1ba76a7e0445868533ed4d64ccd840b85b237d3f
user <user@es302n06>
2017-04-11 15:27:41 +0800

    印出static變數值的寫法

--- src/statik/Tester.java
+++ src/statik/Tester.java
@@ -5,6 +5,7 @@ import statik.Student;
 public class Tester {
 
 	public static void main(String[] args) {
+		System.out.println("及格標準: "+Student.pass);
 		Student s1=new Student(51,95,88);
 		Student s2=new Student(85,59,96);
 		s1.print();
