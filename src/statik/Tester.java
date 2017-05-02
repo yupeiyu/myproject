3c6d4748953e5ef8add936ee376343575cc57f76
user <user@es302n06>
2017-05-02 16:34:29 +0800

    ArrayList

--- src/statik/Tester.java
+++ src/statik/Tester.java
@@ -1,12 +1,14 @@
 package statik;
 
-import statik.Student;
+import java.util.ArrayList;
+
+
 
 public class Tester {
 
 	public static void main(String[] args) {
 		System.out.println("及格標準: "+Student.pass);
-		Student[] s= new Student[3];  //一個陣列放3 個以Student為類別的物件
+		/*Student[] s= new Student[3];  //一個陣列放3 個以Student為類別的物件
 		s[0]=new Student(56,88,98);
 		s[1]=new Student(92,56,81);
 		s[2]=new Student(88,95,77);
@@ -15,7 +17,18 @@ public class Tester {
 		s[1].print();
 		s[2].print();
 		GraduateStu g =new GraduateStu(88,45,69);
-		g.print();
+		g.print();*/
+		System.out.println("==========");
+		
+		ArrayList<Student> list = new ArrayList();
+		list.add(new Student(55,89,78));
+		list.add(new Student(65,85,84));
+		list.add(new GraduateStu(58,66,98));
+		
+		for(int i=0;i<list.size();i++){
+		Student stu =  list.get(i);
+		stu.print();
+		}
 	}
 	
 
