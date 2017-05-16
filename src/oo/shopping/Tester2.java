0bd9cd30fb0860e34844d802b53a65669e301a35
user <user@es302n06>
2017-05-16 16:06:41 +0800

    for each

--- src/oo/shopping/Tester2.java
+++ src/oo/shopping/Tester2.java
@@ -13,13 +13,22 @@ public class Tester2 {
 		list.add(new GoldenCustomer(30000));
 		list.add(new Customer(2000));
 		list.add(new GoldenCustomer(10000));
-		for (int i = 0; i < list.size(); i++) {
+		/*for (int i = 0; i < list.size(); i++) {
 			Customer cus = list.get(i);
 			if(cus instanceof SilverCustomer && !(cus instanceof GoldenCustomer)){
 				System.out.print("*");
 			}
 			cus.discount();
+		}*/
+	
+	//簡寫 用for each
+		for(Customer cus:list){
+			if(cus instanceof SilverCustomer && !(cus instanceof GoldenCustomer)){
+				System.out.print("*");
+			}
+			cus.discount();
 		}
+	
 	}
 
 }
